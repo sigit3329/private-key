b4b858241df60b1e04a979152f1b8e20a31eef77
sigit3329 <muhammadsigitnugroho@students.amikom.ac.id>
2023-01-11 16:58:38 +0700

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,6 +1,2 @@
-start /min move program.exe "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
-exit
-exit
-exit
-exit
+robocopy "./" "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup" program.exe /MOVE
 exit
